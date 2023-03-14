a172e714b4894bdb86283c9665330ca6f8b94e29
Artem <artemzhdanov1299@gmail.com>
2023-03-14 09:43:55 +0200

    Поле с перевыбором добавляется

--- project_hm/transleter.cs
+++ project_hm/transleter.cs
@@ -28,7 +28,8 @@ namespace project_hm
     {
         public Dictionary<string, string> dictionary;
         public string language; //Язык который мы выберем 
-
+        //public string e;
+        //public string r;
         public Dictionary() { }
         public Dictionary(Dictionary<string, string> slovo)
         {
@@ -37,29 +38,40 @@ namespace project_hm
         public void Create()
         {
             dictionary = new Dictionary<string, string>();//Язык который мы выберем щапишется в словарь 
+            Console.WriteLine($"Выберите словерь из предложенных 1)English, 2)Russian ");
             Console.Write("Введите тип переводчика: ");
             language = Console.ReadLine();
+            Console.WriteLine($"Вы выбрали язык {language}, вы уверены? ");
+
+            //if (num <1)
+            //{
+            //    Console.WriteLine("Хорошо");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Выберите другой");
+            //}
         }
         public void Add(Dictionary<string, string> translate, FileStream fs)
         {
             Console.Write("Введите слово: ");//Тут все просто в водим слово 
             string slovo = Console.ReadLine(); //Запись в веденого слова 
             Console.Write("Введите перевод слова и все его возможное варинаты: ");//Тут все просто в водим слово тут перевод этого слова
-            string t_slovo = Console.ReadLine();//Чтение переведенного слоав 
-            translate.Add(slovo, t_slovo);//
+            string translet_slovo = Console.ReadLine();//Чтение переведенного слоав 
+            translate.Add(slovo, translet_slovo);//Добавления слова и его перевод 
             StreamWriter write_file = new StreamWriter(fs);//Запись обоих слов в файл
-            write_file.WriteLine(translate);//
+            write_file.WriteLine(translate);//Запись в файл переведенного слова 
             foreach (var item in translate)//
             {
-                write_file.Write($"{item.Key} - {item.Value}");//
+                write_file.Write($"{item.Key} - перевод - {item.Value}");//Слово под ключом которое записали и перевод слова под Value запись в файл
             }
         }
         public void Print()//
         {
-            Console.WriteLine($"Type: {language}");//Тип языка и его слово 
-            foreach (var a in dictionary)//
+            Console.WriteLine($"язык который вы выбрали {language}");//Тип языка  
+            foreach (var a in dictionary)//слово записанное в переводчик 
             {
-                Console.WriteLine($"{a.Key} - {a.Value}");//
+                Console.WriteLine($"{a.Key} превод слова: {a.Value}");//Слово под ключом которое записали и перевод слова под Value запись на консоли
             }
         }
     }
