db7e3a300d4dffcd57f5f922f969878ca4cbe519
Artem <artemzhdanov1299@gmail.com>
2023-03-14 09:49:29 +0200

    //

--- project_hm/Program.cs
+++ project_hm/Program.cs
@@ -11,12 +11,14 @@ namespace project_hm
     {
         static void Main(string[] args)
         {
+        //////////Идея в том чтобы создать колекцию где выбераешь язык из предложенных 
         //    Dictionary<int, string> BestLanguage = new Dictionary<int, string>()//Создаем колекцию
         //{    //Здесь словарь BestLanguage в качестве ключей принимает значения типа int, а в качестве значений - строки.
         //    {1, "English"},//ключ 1 
         //    {2, "Russian"},//ключ 2
         //};
         //    BestLanguage.Add(3, "German");//Мы можем добавить в колекцию еще один язык //добавляем ключ 3
+        ////////////
             Dictionary translet = new Dictionary();
             translet.Create();//Создаем перевод 
             FileStream file = new FileStream(translet.language + ".txt", FileMode.OpenOrCreate);//Здесь передаеться путь к файлу и перечисление(указывает на режим доступа к файлу)
